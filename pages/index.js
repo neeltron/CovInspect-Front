266198c5be3fe9b722fa9c9aec77c228bd271ac9
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2021-05-09 16:56:00 +0530

    2nd commit

--- pages/index.js
+++ pages/index.js
@@ -24,16 +24,22 @@ export default function Home() {
        </div>
        
        <form className='flex'>
-     
-    <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe"/>
+       <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
     </div>
-  
-        
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
         <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' id='name' type='text' aria-label='name' placeholder='Enter your name' />
          
-         
-        <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' id='text' type='text' aria-label='text1' placeholder='text 1' />
+         <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' id='text' type='text' aria-label='text1' placeholder='text 1' />
          
         <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' id='text' type='text' aria-label='text2' placeholder='text 2' />
          
